9fc7c0dcf486366881e092d716de135e504da637
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-26 11:02:43 -0700

    Finish adding iteration two TDD

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,6 +5,9 @@ require "./lib/attendee"
 RSpec.describe Auction do
   before(:each) do
     @auction = Auction.new
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
     @item3 = Item.new('Homemade Chocolate Chip Cookies')
@@ -37,6 +40,51 @@ RSpec.describe Auction do
       expect(@item1.bids).to eq([])
       @item1.add_bid(attendee2, 20)
       @item1.add_bid(attendee1, 22)
+      expect(@item1.bids).to eq([@attendee2, @attendee1])
+    end
+
+    it "can check for high bids" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      @item1.add_bid(attendee2, 20)
+      @item1.add_bid(attendee1, 22)
+
+      expect(@item1.current_high_bid).to eq(22)
+    end
+
+    it "can check for unpopular items" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
+    end
+
+    it "can check for potentail revenue" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.potential_revenue).to eq(87)
     end
 
   end
