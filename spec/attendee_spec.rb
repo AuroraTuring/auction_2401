bbb0c4c9715473a4695320a89f9836e71760b8c1
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-26 10:25:28 -0700

    Add global @

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -5,6 +5,8 @@ require "./lib/attendee"
 RSpec.describe Attendee do
   before(:each) do
     @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
   it "exists and has attributes" do
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe Auction do
     @auction = Auction.new
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
   end
 
   it "exists and has attributes" do
