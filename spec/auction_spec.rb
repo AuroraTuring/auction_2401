75415f0478de35cb64d79d13867cfc36d4e3e708
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-26 10:37:56 -0700

    Begin #add_bid

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -26,4 +26,19 @@ RSpec.describe Auction do
     expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
 
+  describe "#add_bid" do
+    it "can bid on items" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      expect(@item1.bids).to eq([])
+      @item1.add_bid(attendee2, 20)
+      @item1.add_bid(attendee1, 22)
+    end
+
+  end
+
 end
