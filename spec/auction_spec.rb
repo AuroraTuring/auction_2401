587917db9b7314d3b3f943e3c0492fd7dc47ca96
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-26 10:10:37 -0700

    Add @

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,18 +5,21 @@ require "./lib/attendee"
 RSpec.describe Auction do
   before(:each) do
     @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
 
   it "exists and has attributes" do
-    expect(@auction).to eq(Auction)
+    expect(@auction).to be_a(Auction)
     expect(@auction.items).to eq([])
   end
 
   it "can add items to auction" do
-    @auction.add_item(item1)
-    @auction.add_item(item2)
+    expect(@auction.items).to eq([])
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
 
-    expect(@auction.items).to eq(item1, item2)
+    expect(@auction.items).to eq(@item1, @item2)
     expect(@auction.item_names).to eq("Chalkware Piggy Bank", "Bamboo Picture Frame")
   end
 
